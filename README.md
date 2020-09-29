a3c207fe523306d6a5adc44e270e8ed2946e44e0
cjbrock <corinna@flatironschool.com>
2020-09-29 11:22:24 -0400

    create new tables

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+# Sinatra CRUD
 
+# Routes to build
+
+| HTTP Verbs  |  Routes         | Actions  |
+| GET         | /posts          | index    |
+| GET         | /posts/:id      | show     |
+| GET         | /posts/new      | new      |
+| GET         | /posts/:id/edit | edit     |
+| POST        | /posts          | create   |
+| DELETE      | /posts/:id      | delete   |
+| PUT/PATCH   | /posts/:id      | update   | 
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_152726) do
+ActiveRecord::Schema.define(version: 2020_09_29_151903) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
